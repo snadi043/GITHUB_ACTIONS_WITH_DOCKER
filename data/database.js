db81645d9c94ef804da112dc5e4f522f4ce8ef78
snadi043 <snadi043@uottawa.ca>
2025-08-23 09:40:45 -0400

    checking.

--- data/database.js
+++ data/database.js
@@ -1,11 +1,4 @@
-// import { MongoClient, ServerApiVersion } from 'mongodb';
-
-// Importing the installed MongoDB pacakge to use in the application to integrate with the database.
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient; //Configuring MongoClient to connect to mongoDB.
-
-let _db;
-
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 // const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 // const dbName = process.env.MONGODB_DB_NAME;
@@ -13,55 +6,30 @@ let _db;
 // const dbUser = process.env.MONGODB_USERNAME;
 // const dbPassword = process.env.MONGODB_PASSWORD;
 
-// Inorder to use the connection process as a reusable code, calling it with a method to export it and use in the application.
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://snadi043:glC28pIZf3JtErdR@cluster0.ierxn0t.mongodb.net/github-actions-docker-container?retryWrites=true&w=majority') //shop is the database name.
-  .then(client => {
-    _db = client.db();
-    callback();
-    console.log('Connected!');
-  })
-  .catch(err => {
-    console.log(err);
-    throw err;
-  });
+// const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = 'mongodb+srv://snadi043:glC28pIZf3JtErdR@cluster0.ierxn0t.mongodb.net/github-actions-docker-container?retryWrites=true&w=majority'
+const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+console.log('Trying to connect to db');
+
+try {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log('Connected successfully to server');
+} catch (error) {
+  console.log('Connection failed.');
+  await client.close();
+  console.log('Connection closed.');
+  process.exit(1);
 }
 
-// Storing the access to the database to efficently use the database connection.
-const getDb = () => {
-  if(_db){
-    return _db;
-  }
-  throw 'No database connection found';
-}
+const database = client.db(dbName);
 
-// Exporting the connection process.
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
-
-// const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/${dbName}?retryWrites=true&w=majority`
-// const client = new MongoClient(uri, {
-//         serverApi: {
-//             version: ServerApiVersion.v1,
-//             strict: true,
-//             deprecationErrors: true,
-//         }
-//     });
-
-// console.log('Trying to connect to db');
-
-// try {
-//   await client.connect();
-//   await client.db(dbName).command({ ping: 1 });
-//   console.log('Connected successfully to server');
-// } catch (error) {
-//   console.log('Connection failed.');
-//   await client.close();
-//   console.log('Connection closed.');
-//   process.exit(1);
-// }
-
-// const database = client.db(dbName);
-
-// export default database;
+export default database;
 
--- routes/test.js
+++ routes/test.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import getDb from '../data/database.js';
+import db from '../data/database.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const allTests = await getDb.collection('test').find().toArray();
+  const allTests = await db.collection('test').find().toArray();
   res.json({ test: allTests });
 });
 
 router.post('/', async (req, res) => {
   const testData = req.body;
-  const result = await getDb.collection('test').insertOne({...testData});
+  const result = await db.collection('test').insertOne({...testData});
   res.status(201).json({
     message: 'Test created.',
     test: { ...testData, id: result.insertedId },
